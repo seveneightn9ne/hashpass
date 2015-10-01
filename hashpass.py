f71464000d8becbea2abbe17e617d8056760c744
Miles Steele <miles@milessteele.com>
2015-10-01 19:38:59 -0400

    add pinentry

--- hashpass.py
+++ hashpass.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from hashpasslib import *
+import pinentry
 
 import os.path, hashlib, getpass, sys, pyperclip
 from subprocess import Popen, PIPE
@@ -10,13 +11,28 @@ def init():
         save_master(getpass.getpass("No master password found. Enter one now: "))
         print "Your master password will be hashed and saved at " + MASTER_PW_PATH
 
-def get_password():
+def get_password_cli():
     """ Gets the password via CLI and makes sure it matches the stored password """
     pw = getpass.getpass("Enter master password: ")
     while not is_correct_master(pw):
         print "That doesn't match the stored master."
         pw = getpass.getpass("Try again: ")
     # when is_correct_master(pw) it will be saved
+
+def get_password():
+    """ Gets the password via pinentry if possible fallback to CLI. """
+    try:
+        pw = pinentry.get_pin(description="Enter hashpass master password:",
+                              prompt="Password:")
+        while pw is None or not is_correct_master(pw):
+            if pw == None:
+                print "Bye."
+                sys.exit(0)
+            pw = pinentry.get_pin(description="Enter hashpass master password:",
+                                  prompt="Password:",
+                                  errormsg="That doesn't match the stored master.")
+    except pinentry.PinEntryException:
+        return get_password_cli()
 
 def send_to_clipboard(text):
     pyperclip.copy(text)
---
+++ pinentry.py
@@ -0,0 +1,51 @@
+import subprocess
+
+class PinEntryException(Exception):
+    """Error while getting pin entry."""
+
+def get_pin(description="", prompt="", errormsg=""):
+    """Run pinentry to get a password from the user.
+    
+    Return value:
+        User password as a string. Could be empty.
+        None if the user pressed cancel.
+    
+    Raises:
+        PinEntryException if something went wrong communicating with pinentry.
+    """
+    try:
+        pinentry = subprocess.Popen(["pinentry"],
+                                    stdin=subprocess.PIPE,
+                                    stdout=subprocess.PIPE)
+    except OSError:
+        # Could not launch pinentry.
+        raise PinEntryException("Could not launch pinentry")
+
+    commands = ""
+    commands += "SETDESC {}\n".format(description)
+    commands += "SETPROMPT {}\n".format(prompt)
+    commands += "SETERROR {}\n".format(errormsg)
+    commands += "OPTION grab\n"
+    commands += "GETPIN\n"
+    commands += "BYE\n"
+
+    output, _ = pinentry.communicate(input=commands)
+    output_lines = output.split("\n")
+
+    # Verify output.
+    if len(output_lines) < 8:
+        raise PinEntryException("Unexpected pinentry process output.")
+
+    xline = output_lines[-4]
+    yline = output_lines[-3]
+    if xline[:2] == "D ":
+        # User entered valid input.
+        return xline[2:]
+    elif yline[:3] == "ERR":
+        # User cancelled.
+        return None
+    elif xline == "OK":
+        # User entered empty string.
+        return ""
+    else:
+        raise PinEntryException("Unexpected pinentry process output.")
