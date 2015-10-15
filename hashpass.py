86e8ec61cc920e757c6ac0e7091f687deb5034af
Miles Steele <miles@milessteele.com>
2015-10-15 10:25:54 -0400

    add bcrypt

--- hashpass.py
+++ hashpass.py
@@ -16,19 +16,20 @@ from subprocess import Popen, PIPE
 
 def init():
     """ Saves the master password to disk if you haven't already """
-    if not get_stored_master():
-        save_master(getpass.getpass("No master password found. Enter one now: "))
+    if not read_stored_master():
+        secret_master = getpass.getpass("No master password found. Enter one now: ")
+        store_master(secret_master)
         print "Your master password will be hashed and saved at " + MASTER_PW_PATH
 
-def get_password_cli():
+def get_password_cli(use_bcrypt):
     """ Gets the password via CLI and makes sure it matches the stored password """
     pw = getpass.getpass("Enter master password: ")
     while not is_correct_master(pw):
         print "That doesn't match the stored master."
         pw = getpass.getpass("Try again: ")
-    # when is_correct_master(pw) it will be saved
+    use_master(pw, use_bcrypt)
 
-def get_password():
+def get_password(use_bcrypt):
     """ Gets the password via pinentry if possible. Fallback to CLI. """
     try:
         pw = pinentry.get_pin(description="Enter hashpass master password:",
@@ -40,6 +41,7 @@ def get_password():
             pw = pinentry.get_pin(description="Enter hashpass master password:",
                                   prompt="Password:",
                                   errormsg="That doesn't match the stored master.")
+        use_master(pw, use_bcrypt)
     except pinentry.PinEntryException:
         return get_password_cli()
 
@@ -49,11 +51,13 @@ def send_to_clipboard(text):
 
 def cli(arguments):
     """ runs the app with the CLI as the user interface """
+    use_bcrypt = arguments['--bcrypt']
+
     init()
-    get_password()
+    get_password(use_bcrypt)
     if arguments['<website>']:
         w = arguments['<website>']
-        result = make_password(w, arguments['--bcrypt'])
+        result = make_password(w)
         if arguments['--show']:
             print result
         else:
@@ -68,7 +72,7 @@ def cli(arguments):
                 return
             if w == "":
                 return
-            result = make_password(w, arguments['--bcrypt'])
+            result = make_password(w)
             if arguments['--show']:
                 print result
             else:
--- hashpasslib.py
+++ hashpasslib.py
@@ -7,41 +7,47 @@ MASTER_PW_FILE = "password.bcrypt"
 MASTER_PW_PATH = MASTER_PW_DIR + MASTER_PW_FILE
 
 session_master = None
+session_intermediate = None
 
-def get_stored_master():
+def use_master(master_plaintext, use_bcrypt=False):
+    global session_master
+    global session_intermediate
+    session_master = master_plaintext
+    if use_bcrypt:
+        session_intermediate = alg.make_intermediate(session_master)
+    else:
+        session_intermediate = session_master
+
+def read_stored_master():
     """
     Gets the stored component of the master saved on disk at MASTER_PW_PATH.
     If that file doesn't exist, returns None.
     """
     return open(MASTER_PW_PATH, 'r').read() if os.path.exists(MASTER_PW_PATH) else None
 
-def save_master(master_plaintext):
-    """ Saves the SHA512 of master_plaintext to MASTER_PW_PATH. """
+def store_master(master_plaintext):
+    """Safely stores a derivation of the master to MASTER_PW_PATH for checking against."""
     os.makedirs(MASTER_PW_DIR)
     with os.fdopen(os.open(MASTER_PW_PATH, os.O_WRONLY | os.O_CREAT, 0600), 'w') as f:
         f.write(alg.make_storeable(master_plaintext))
-    global session_master
-    session_master = master_plaintext
 
 def is_correct_master(password):
     """ Returns True if the password matches the stored master, else False. """
     global session_master
     if session_master:
         return session_master == password
-    stored_component = get_stored_master()
+    stored_component = read_stored_master()
     if (stored_component is not None
         and alg.check_stored(password, stored_component)):
         session_master = password
         return True
     return False
 
-def make_password(slug, use_bcrypt=False):
+def make_password(slug):
     """
     Turns the password + slug into a 20 character password.
     The password is_good_pass and is deterministic.
     """
-    if use_bcrypt:
-        raise Exception("Bcrypt is not yet supported.")
-    if not session_master:
-        raise Exception("Cannot make password without a master pw")
-    return alg.make_site_password(session_master, slug)
+    if not session_intermediate:
+        raise Exception("Cannot make password without an intermediate pw.")
+    return alg.make_site_password(session_intermediate, slug)
